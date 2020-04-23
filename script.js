fcb95e9c1510bdaad3f3383af028cdd502635a70
kimSpiegel04 <k.spiegel72@gmail.com>
2020-04-23 15:02:20 -0400

    tinkering with script

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 var board;
+const choices = [1,2,3,4,5,6,7,8,9];
 const cells = document.querySelectorAll('.cell');
 const box_1 = document.querySelectorAll('.b-1');
-console.log(box_1);
 
 //rules
-// function startBoard(){
-//     if()
-// }
+for(var property in box_2) {
+    if (box_2.hasOwnProperty(property)) {
+        console.log(property);
+    }
+}
 
 function startGame(){
-    board = Array.from(Array(81).keys())
+    board = Array.from(Array(9).keys())
     for (var i=0; i<cells.length; i++){
         cells[i].innerText = '';
         cells[i].addEventListener('click', addNum, false);
     }
-    console.log(board);
+    // console.log(board);
     // addPuzzle();
 }
 
@@ -25,6 +27,5 @@ function startGame(){
 function addNum(){
     console.log(this)
 }
-
 
 startGame();
